956dfe577a63321e4b56d05112c00f638f55e07d
KekahiSensei <147841412+KekahiSensei@users.noreply.github.com>
2023-12-15 11:31:48 -0600

    Removed select initiative dialog after rules review.

--- Play Card.js
+++ Play Card.js
@@ -11,13 +11,18 @@ let card1 = playedCards[0]._source.cards[0].name;
 let card2 = playedCards[0]._source.cards[1].name;
 let card1Init = playedCards[0]._source.cards[0].value;
 let card2Init = playedCards[0]._source.cards[1].value;
+let initiative = Math.min(card1Init, card2Init);
 
+//for testing
 console.log("User Name is: " + userName);
 console.log("User Actor is: " + userActor[0].name);
 console.log("Revealed Hand is: " + revealedHand);
 console.log("Played cards are: " + card1 + " and " + card2);
 console.log("Card 1 initiative is: " + card1Init);
 console.log("Card 2 initiative is: " + card2Init);
+console.log("Initiative is: " + initiative);
+
+playCard();
 
 function playCard () {
 
@@ -27,17 +32,17 @@ if (revealedHand[0]._source.cards.length != 2) {
 
 let confirmHandDialog = new Dialog({
     title: "Play Hand",
-    content: "Do you wish to play this hand?" + card1 + card2,
+    content: "Do you wish to play this hand? " + card1 + " and " + card2 + " Initiative: " + initiative,
     buttons: {
         one: {
             icon: '<i class="fas fa-check"></i>',
             label: "Yes",
-            callback: () => getInitiative(card1Init, card2Init)
+            callback: () => setInitiative()
         },
         two: {
             icon: '<i class="fas fa-times"></i>',
             label: "No",
-            callback: () => console.log("Chose Two")
+            callback: () => console.log("Chose no")
         }
     },
     default: "two",
@@ -49,33 +54,8 @@ confirmHandDialog.render(true);
     }  //end if-else
 } //end playCard function
 
-playCard();
-
-function getInitiative (init1, init2) {
-    let getInitDialog = new Dialog ({
-        title: "Pick Initiative",
-        content: "Which initiative do you want?",
-        buttons: {
-            one: {
-                icon: '<i class=" ra-crossed-swords"></i>',
-                label: init1,
-                callback: () => setInitiative(init1)
-            },
-            two: {
-                icon: '<i class=" ra-crossed-swords"></i>',
-                label: init2,
-                callback: () => setInitiative(init2)
-            }
-        },
-        default: "two",
-        render: html => console.log("Register interactivity in the rendered dialog"),
-        close: html => console.log("This always is logged no matter which option is chosen")
-    });
-    getInitDialog.render(true);
-}
-
-function setInitiative (initiative) {
-    let combat = game.combats.combats[0];
+function setInitiative () {
+    //let combat = game.combats.combats[0];
     game.combat.setInitiative(combatantId, initiative)
     console.log("set " + userName + "initiative to " + initiative);
 }
